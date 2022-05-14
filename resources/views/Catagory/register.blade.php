e20ada2bd0f1519188f3b91ee96ba8cd30708578
abere <aberemihrete1@gmail.com>
2022-05-14 21:05:54 +0300

    e

--- resources/views/Catagory/register.blade.php
+++ resources/views/Catagory/register.blade.php
@@ -6,7 +6,7 @@
  <div class="row justify-content-center">
  <div class="col-md-8">
  <div class="card">
- <div class="card-header">{{ __('Catagory Registration') }}</div>
+ <div class="card-header">{{ __('Catagoies Registration') }}</div>
  <div class="card-body">
  <form method="POST" action="{{ route('Catagory/register') }}"
 >
